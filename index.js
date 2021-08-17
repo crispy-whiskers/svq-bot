3d5551a7028fe7a46eb385646a95ac4294a35055
ladsfsdfs <nguyen.k.allen@gmail.com>
2021-08-17 00:26:36 -0400

    another fun commnad

--- index.js
+++ index.js
@@ -7,12 +7,16 @@ bot.on('message', (message)=>{
     if(/(when.+?(ch(\d|$| )|chapter|chap)|(ch(\d|$| )|chapter|chap).*?when)/gi.test(message.content)){
         count=count+1;
         message.reply(
-            `I detected that you may have asked about when a new chapter will release. If you did ask, please read <#802339257540149248>.
+            `I detected that you may have asked about when a new chapter will release. If you did ask, please read <#872530946585419899>.
 Since this bot was started, this has happened \`${count}\` times.`);
         return;
     }
     if(message.content === 'punch timka'){
         message.channel.send('poonch')
+    }
+
+    if(message.content === "kiss mwah"){
+        message.channel.send('u lookin kinda breedable ngl');
     }
 })
 
